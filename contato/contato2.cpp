fdff123a46e64a941a1b1e04515d6ea5366cfb3a
AnnyCarolineS <39037423+AnnyCarolineS@users.noreply.github.com>
2018-09-26 18:34:53 -0300

    Add files via upload

--- contato/contato2.cpp
+++ contato/contato2.cpp
@@ -18,6 +18,16 @@ struct Fone{
         ss<< operadora << ":"<<telefone;
         return ss.str();
     }
+    static bool validate(string telefone){
+        for(int i=0;i<(int)telefone.size();i++){
+            if(telefone[i]=='0'||telefone[i]=='1' ||telefone[i]=='2'||telefone[i]=='3'||telefone[i]=='4'||telefone[i]=='5'||telefone[i]=='6'||telefone[i]=='7'||telefone[i]=='8'||telefone[i]=='9'||telefone[i]=='('||telefone[i]==')'||telefone[i]=='.'){
+                continue;
+            }
+            return false;
+        }
+        return true;
+        
+    }
 };
 struct Contato{
     
@@ -38,18 +48,8 @@ struct Contato{
         }  
         return ss.str();
     }
-    bool validate(string telefone){
-        for(int i=0;i<(int)telefone.size();i++){
-            if(telefone[i]=='0'||telefone[i]=='1' ||telefone[i]=='2'||telefone[i]=='3'||telefone[i]=='4'||telefone[i]=='5'||telefone[i]=='6'||telefone[i]=='7'||telefone[i]=='8'||telefone[i]=='9'||telefone[i]=='('||telefone[i]==')'||telefone[i]=='.'){
-                continue;
-            }
-            return false;
-        }
-        return true;
-        
-    }
     bool add(string operadora, string telefone){
-        if(validate(telefone)){
+        if(Fone::validate(telefone)){
             fone.push_back(new Fone(operadora,telefone));
             return true;
         }
@@ -62,16 +62,34 @@ struct Contato{
         fone.erase(fone.begin()+indice);
         return true;
     }
+    bool update(string linha){
+        stringstream ss(linha);
+        fone.clear();
+        string txt;
+        Fone fone;
+        while(ss>>txt){ 
+            stringstream ss(txt);
+            string operadora,telefone;
+            getline(ss,operadora,':');
+            getline(ss,telefone,' ');
+            if(Fone::validate(telefone)){
+                add(operadora,telefone);
+            }
+        }
+        return true;
+    } 
 
 };
 
 int main(){
     Contato contato;
     string opcao;
+    string txt;
     
 
     while(true){
         cin>>opcao;
+
         if(opcao=="init"){
             string nome;
             cin>>nome;
@@ -81,7 +99,6 @@ int main(){
             cout<<contato.toString()<<endl;
 
         }else if(opcao=="add"){
-
             string operadora;
             string telefone;
             cin>>operadora>>telefone;
@@ -91,7 +108,7 @@ int main(){
                 cout<<"failure: Fone invalido"<<endl;
             }
         }else if(opcao=="help"){
-            cout<<"init - show - add - rm - help \n";
+            cout<<"init - show - add - rmv - update - help \n";
         }else if(opcao=="rmv"){
             int indice;
             cin>>indice;
@@ -101,6 +118,19 @@ int main(){
                 cout<<"failure : Indice invalido"<<endl;
             }
 
+        }else if(opcao=="update"){
+            string line;
+            getline(cin,line);
+            stringstream ss(line);
+            string palavra;
+            ss >> palavra;
+            contato=Contato(palavra);
+
+            string up;
+            getline(ss,up);
+            cout<<up<<endl;
+            if(contato.update(up))
+            cout<<"success"<<endl;
         }
     }
 
